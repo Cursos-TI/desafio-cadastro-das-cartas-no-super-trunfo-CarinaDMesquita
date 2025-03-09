372b01f6d46e32700ed9cacf207109533352c9f4
CarinaDMesquita <carina27dias@gmail.com>
2025-03-09 20:20:38 +0000

    desafioSuperTrunfoAjuste5.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,43 +8,54 @@
 
 int main() {
 
-    // Variáveis declaradas e atributos das cidades. 
-    // Esses são os atributos: estado, código da cidade, nome, população, área, PIB, número de pontos turísticos.
-
-    char estado1; 
-    char cod_cidade1[4]; 
-    char nome_cidade1[65]; 
-    int habitantes1; 
-    float area_km1; 
-    float pib1; 
-    int numeros_pontos_turistico1;
-
-    char estado2[2];
-    char cod_cidade2[4];
-    char nome_cidade2[65];
-    int habitantes2;
-    float area_km2;
-    float pib2; 
-    int numeros_pontos_turistico2;
-
-
     // Inicio do jogo 
-
     printf("OLÁ SEJA BEM VINDO(A) AO JOGO DE CARTAS SUPER TRUNFO!!!\n\n");
     printf("VAMOS COMEÇAR? BOA SORTE E ÓTIMO JOGO!!!\n\n");
 
+    /*  
+        Atributos para as duas Cartas: estado, código da cidade, nome da cidade, habitantes
+        área, PIB, número de pontos turísticos, densidade populacional, PIB per Capita, super poder. 
+    */
 
-    // Cadastro CARTA 1:
-    // Função printf para a saída de dados fornecendo as informações ao usuário sobre os atributos da cidade Carta 1.
-    //Função scanf capturando as entradas do usuário para cada atributo da cidade Carta 1.
+    // Variáveis declaradas Carta1 com os atributos da cidade.
+    char estado1; 
+    char cod_cidade1[4]; 
+    char nome_cidade1[65]; 
 
+    int habitantes1; 
+    int numeros_pontos_turistico1;
+
+    float area_km1; 
+    float pib1; 
+    float densidadePopu1;
+    float perCapita1;
+
+    // Variáveis declaradas Carta2 com os atributos da cidade.
+    char estado2[2];
+    char cod_cidade2[4];
+    char nome_cidade2[65];
+
+    int habitantes2;
+    int numeros_pontos_turistico2;
+
+    float area_km2;
+    float pib2; 
+    float densidadePopu2;
+    float perCapita2;
+
+    /* 
+        Cadastro CARTA 1:
+        Função printf para a saída de dados fornecendo as informações ao usuário sobre os atributos 
+        da cidade Carta 1.
+        Função scanf capturando as entradas do usuário para cada atributo da cidade Carta 1.
+    */
     printf("CARTA 1\n");
 
     printf ("ATENÇÃO: A letra dever ser maiúscula e deve conter apenas uma letra de 'A' à 'H'.\n");
     printf("Informe o Estado:\n");
-    scanf("%c", &estado1);
+    scanf(" %c", &estado1);
         
-    printf ("ATENÇÂO: O Código da cidade deve conter a letra que foi digitada anteriormente para o estado seguida do número 01 à 04. \n");
+    printf ("ATENÇÂO: O Código da cidade deve conter a letra que foi digitada anteriormente para o estado seguida do número 01 à 04.\n");
     printf("Informe o Código da Cidade:\n"); 
     scanf("%s", &cod_cidade1);
 
@@ -62,20 +73,28 @@ int main() {
 
     printf("Informe quantos pontos túristicos tem nessa Cidade:\n");
     scanf("%d", &numeros_pontos_turistico1);
-    printf("\n\n");
-
     
-    // Cadastro CARTA 2:
-    // Função printf para a saída de dados fornecendo as informações ao usuário sobre os atributos da cidade Carta 2.
-    //Função scanf capturando as entradas do usuário para cada atributo da cidade Carta 2.
+    // Conversão Explícita (Casting)
+    densidadePopu1 = (float)(habitantes1) / area_km1;
+    
+    //Conversão Explícita (Casting)
+    perCapita1 = pib1 / (float)habitantes1;
 
+    printf("\n\n");
+    
+    /* 
+        Cadastro CARTA 2:
+        Função printf para a saída de dados fornecendo as informações ao usuário sobre os atributos 
+        da cidade Carta 2.
+        Função scanf capturando as entradas do usuário para cada atributo da cidade Carta 2.
+    */
     printf("CARTA 2\n");
 
     printf ("ATENÇÂO: A letra dever ser maiúscula e deve conter apenas uma letra de 'A' à 'H'.\n");
     printf("Informe o Estado:\n");
     scanf("%s", &estado2);
 
-    printf ("ATENÇÂO: O código da cidade deve conter a letra do estado seguida do número 01 à 04. \n");
+    printf ("ATENÇÂO: O código da cidade deve conter a letra do estado seguida do número 01 à 04.\n");
     printf("Informe o Código da Cidade:\n"); 
     scanf("%s", &cod_cidade2);
 
@@ -93,7 +112,14 @@ int main() {
 
     printf("Informe quantos pontos túristicos tem nessa Cidade:\n");
     scanf("%d", &numeros_pontos_turistico2);
-    printf("\n\n");
+   
+    // Conversão Explícita (Casting)
+    densidadePopu2 = (float)(habitantes2) / area_km2;
+
+    //Conversão Explícita (Casting)
+    perCapita2 = pib2 / (float)habitantes2;
+
+    printf("\n");
     
     printf("RESULTADO FINAL\n\n");
 
@@ -108,8 +134,9 @@ int main() {
     printf("Área: %.2f km²\n", area_km1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Números de pontos Turísticos: %d\n", numeros_pontos_turistico1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopu1);
+    printf("PIB per Capita: %.2f reais\n", perCapita1);
     printf("\n");
-
     
     // Exibição dos dados da CARTA 2:
     // Função printf exibindo as informações e os valores de cada atributo da cidade cadastrada Carta 2 
@@ -122,13 +149,14 @@ int main() {
     printf("Área: %.2f km²\n", area_km2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Números de pontos Turísticos: %d\n", numeros_pontos_turistico2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopu2);
+    printf("PIB per Capita: %.2f reais\n", perCapita2);
     printf("\n");
-
 
     printf("FIM DO JOGO!!! \n\n");
     printf("OBRIGADO(A)!!! \n");
 
-
     return 0;
 
 }
+
